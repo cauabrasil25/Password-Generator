e49156344d942c1a3a832701ab39b79d02b54269
Arduino <arduino@LE-70.lcc.ufrn.br>
2024-08-12 15:34:46 -0300

    function help addiction

---
+++ password-generator.sh
@@ -0,0 +1,11 @@
+help() {
+echo "Welcome to password-generator! Version 1.0, (c) 2024, Cauã and Alexandre, DIMAp, UFRN"
+echo "Use: ./password-generator.sh [OPTIONS]"
+echo "Options:"
+echo "-l [lenght] : password lenght"
+echo "-u          : include uppercase letters"
+echo "-d          : include numbers"
+echo "-s          : include symbols"
+echo "-h          : shows this help message"
+
+echo "The default behavior of the script is to generate a password of 8 lowercase characters."
