f8086bb2320e0b92384b4dc7f53fc331fb14128a
caua brasil <cauabrasil25@gmail.com>
2024-08-13 17:11:14 -0300

    identation

--- password-generator.sh
+++ password-generator.sh
@@ -2,16 +2,16 @@
 
 # Help message
 function help() {
- echo "Welcome to password-generator! Version 1.0, (c) 2024, Cauã and Alexandre, DIMAp, UFRN"
- echo "Use: ./password-generator.sh [OPTIONS]"
- echo "Options:"
- echo "-l [lenght] : password lenght"
- echo "-u          : include uppercase letters"
- echo "-d          : include numbers"
- echo "-s          : include symbols"
- echo "-h          : shows this help message"
- echo ""
- echo "The default behavior of the script is to generate a password of 8 lowercase characters."
+    echo "Welcome to password-generator! Version 1.0, (c) 2024, Cauã and Alexandre, DIMAp, UFRN"
+    echo "Use: ./password-generator.sh [OPTIONS]"
+    echo "Options:"
+    echo "-l [lenght] : password lenght"
+    echo "-u          : include uppercase letters"
+    echo "-d          : include numbers"
+    echo "-s          : include symbols"
+    echo "-h          : shows this help message"
+    echo ""
+    echo "The default behavior of the script is to generate a password of 8 lowercase characters."
 }
 
 # Default values
