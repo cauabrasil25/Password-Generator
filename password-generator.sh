bdaf3a6fabc87a33a6041fa2341b9709ad0a9f25
xandecoh <xandemclm@gmail.com>
2024-08-13 17:27:18 -0300

    Added the list of the allowed characters

--- password-generator.sh
+++ password-generator.sh
@@ -17,11 +17,59 @@ help() {
 # Default values
 LENGTH=8
 USE_UPPERCASE=false
-USE_DIGITS=false
+USE_NUMBERS=false
 USE_SYMBOLS=false
+
+# Analyze arguments
+while getopts "l:udsh" opt; do
+   case ${opt} in
+   l )
+          LENGTH=$OPTARG
+          ;;
+   u )
+          USE_UPPERCASE=true
+          ;;
+   d )      
+          USE_NUMBERS=true
+          ;;
+   s )
+          USE_SYMBOLS=true
+          ;;
+   h )
+          help
+          exit 0
+          ;;
+   \? )   
+          echo "Invalid option: -$OPTARG" >&2
+          help  
+          exit 1
+          ;;
+    :)
+         echo "Option -$OPTARG requires an argument" >&2
+         help
+         exit 1
+         ;;
+
+    esac
+done
 
 # Set characters
 LOWERCASE="abcdefghijklmnopqrstuvwxyz"
 UPPERCASE="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 DIGITS="0123456789"
 SYMBOLS="!@#$%^&*()-_=+[]{}|;:,.<>?/~"
+
+# Build the list of allowed characters 
+CHARSET="$LOWERCASE"
+
+if [ "$USE_UPPERCASE" = true ]; then
+   CHARSET+="$UPPERCASE"
+fi
+
+if [ "$USE_DIGITS" = true ]; then
+   CHARSET+="$DIGITS"
+fi
+
+if [ "$USE_SYMBOLS" = true ]; then
+   CHARSET+="$SYMBOLS"
+fi
